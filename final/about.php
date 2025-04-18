207f74489ca94937a96c88a39516b1ab48b1c9a7
lawistech <junieann.nangkil10@gmail.com>
2025-04-18 21:58:19 +0800

    Add styles for plan and service sections, enhance pricing card design

--- final/about.php
+++ final/about.php
@@ -1,5 +1,6 @@
 <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link rel="stylesheet" href="/assets/css/global.css" />
 
   <!-- Mobile Menu Overlay -->
   <div class="mobile-overlay"></div>
